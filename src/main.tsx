89d5bfea66574f904154402454b64b24cbdee204
esteban_terrones <20223007@aloe.ulima.edu.pe>
2025-01-27 11:30:53 -0500

    avance ventana login, solo diseno

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import LoginPage from './pages/LoginPage'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <LoginPage />
+  </StrictMode>,
+)
